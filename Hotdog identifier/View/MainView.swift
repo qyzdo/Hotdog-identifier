9732f16f7f1fa21c7ecd908b6ba51849762b2604
Oskar Figiel <qyzdo@outlook.com>
2021-01-13 16:19:54 +0100

    Added loading view with rotating extension

--- Hotdog identifier/View/MainView.swift
+++ Hotdog identifier/View/MainView.swift
@@ -36,6 +36,10 @@ final class MainView: UIView {
         hotdogView.rightAnchor.constraint(equalTo: safeArea.rightAnchor).isActive = true
         hotdogView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         hotdogView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+
+        self.addSubview(loadingView)
+        loadingView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        loadingView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -98,4 +102,35 @@ final class MainView: UIView {
         hotdogView.isHidden = true
         return hotdogView
     }()
+
+    public var loadingView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "loading")
+        imageView.isHidden = true
+        return imageView
+    }()
+}
+
+extension UIView {
+    private static let kRotationAnimationKey = "rotationanimationkey"
+
+    func rotate(duration: Double = 2) {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) == nil {
+            let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+
+            rotationAnimation.fromValue = 0.0
+            rotationAnimation.toValue = Float.pi * 2.0
+            rotationAnimation.duration = duration
+            rotationAnimation.repeatCount = Float.infinity
+
+            layer.add(rotationAnimation, forKey: UIView.kRotationAnimationKey)
+        }
+    }
+
+    func stopRotating() {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) != nil {
+            layer.removeAnimation(forKey: UIView.kRotationAnimationKey)
+        }
+    }
 }
